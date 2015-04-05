7b17fa6ee402ffb11c1f8d8ae445183ac60c95a5
Serhii Podvysotskyi <Serhii.podvysotskyi@gmail.com>
2015-04-05 13:56:07 +0300

    migrations

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'forum',
-			'username'  => 'serg',
-			'password'  => 'serg2303',
+			'username'  => 'forum',
+			'password'  => '123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'development' => array('localhost', gethostname()),
+	//'local' => array('homestead'),
 
 ));
 
